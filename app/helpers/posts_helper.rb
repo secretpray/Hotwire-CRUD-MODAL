09b193fbddaf6465236f771de1899110c6886bf9
secretpray <Renovatio100>
2021-12-08 09:10:13 +0200

    Added comments count and author info (all broadcasted)

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -11,4 +11,12 @@ module PostsHelper
       truncate(title, length: 42, separator: ' ')
     end
   end
+
+  def comments_user_data(post)
+    comments_count = post.comments.count
+    comment_users =  User.find(post.comments.pluck(:user_id).uniq)
+    users_avatar = comment_users.map{|user| AvatarPresenter.call(user, 40)}
+
+    return { count: comments_count, users_avatar: users_avatar.join(', ') }
+  end
 end
