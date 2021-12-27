d335661d74980492acb9cb8701d160059d9205ee
secretpray <Renovatio100>
2021-12-27 12:34:04 +0200

    Tooltip experiments #1

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -42,3 +42,6 @@ application.register("object-author", ObjectAuthorController)
 
 import SearchController from "./search_controller.js"
 application.register("search", SearchController)
+
+import TooltipController from "./tooltip_controller.js"
+application.register("tooltip", TooltipController)
