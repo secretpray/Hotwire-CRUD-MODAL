b12b0cdbc63833266d72da6bcf6d475369940239
secretpray <Renovatio100>
2022-01-04 09:46:19 +0200

    infinity scroll after delay with loader timer

--- app/javascript/controllers/endless_controller.js
+++ app/javascript/controllers/endless_controller.js
@@ -1,51 +1,119 @@
 import { Controller } from "@hotwired/stimulus"
-import { get } from "@rails/request.js";
-// import { turboReady } from "../utils/turbo_utils" // wait formSubmission (not needed now)
+import { get } from "@rails/request.js"
+
+let count = null,             // timer duartion
+    running = true,           // loader status
+    mOld = null,              // old timer value
+    mNew = null,              // new timer value
+    options = {               // options for Observer
+                root: null,
+                rootMargins: "0px",
+                threshold: 0.7 // Target overlap percentage (1.0 - 100%)
+    }
+
+const intersections = new Map() // map set for Observer
 
 export default class extends Controller {
-  static targets = [ "footer", "pagination" ]
+
+  static targets = [
+                     "footer",
+                     "current",
+                     "next",
+                     "pagination",
+                     "pagination_prev",
+                     "loader",
+                     "seconds",
+                     "milliseconds"
+                   ]
   static values = {
-    prevpage: String,
-    request: Boolean,
-  }
+                    request: Boolean
+                   }
 
   connect() {
-    this.createObserver()
+    if (document.body.dataset.actionName == 'index') {
+      this.createObserver()
+    }
   }
 
   createObserver() {
-    const observer = new IntersectionObserver(entries => this.handleIntersect(entries),
-      {
-        threshold: [0, 1.0],
-      }
-    )
-    observer.observe(this.footerTarget)
+    const observer = new IntersectionObserver(this.callback, options)
+
+    if (this.hasFooterTarget) { observer.observe(this.footerTarget) }
   }
 
-  handleIntersect(entries) {
-    entries.forEach(entry => {
-      // check: view footer in index page
-      if (entry.isIntersecting && document.body.dataset.actionName == 'index') {
-        this._loadMore()
-      }
-    })
+
+  draw = () => {
+    if (count > 0 && running) {
+      requestAnimationFrame(() => {
+        this.draw()
+      })
+      mNew = new Date().getTime();
+      count = count - mNew + mOld;
+      count = count >= 0 ? count : 0;
+      mOld = mNew;
+      this.secondsTarget.innerText = Math.floor(count / 1000);
+      this.millisecondsTarget.innerText = count % 1000;
+    } else {
+      this.secondsTarget.innerText = 0
+      this.millisecondsTarget.innerText = 0
+    }
   }
 
-  async _loadMore() {
-    // return if busy by prev request or !has next page link
-    if (this.requestValue || !this.hasPaginationTarget) {
+  // little usability hack: add page 1 link if all page added and prevPage.href = null
+  firstLinkAddIfNeeded = () => {
+    let service = this.nextTarget.dataset['service']
+    let result = Number(this.currentTarget.dataset['totalpage']) - Number(service)
+    if (result < 2 && !this.hasPagination_prevTarget) {
+      const firstUrl = this.currentTarget.dataset['prevpageLinkCurrent']
+      const prevElement = document.getElementById('pagination-prev')
+      prevElement.removeAttribute('disabled')
+      prevElement.href = firstUrl
+      prevElement.innerText = '1'
+      prevElement.classList.remove('hover:opacity-25', 'cursor-default')
+      prevElement.classList.add('hover:opacity-100')
+    }
+  }
+
+  _loadMore = async () => {
+  // _loadMore = () => {
+    if (!this.hasPaginationTarget || this.requestValue) {
       return
     }
 
-    const prevPage = this.paginationTarget.dataset['endlessPrevpageValue']
-    const endlessUrl = new URL(this.paginationTarget.href)
-    // endlessUrl.searchParams.set("endless", prevPage)
-    endlessUrl.searchParams.append("endless", prevPage)
-    // busy
     this.requestValue = true
+    const prevPageValue = this.currentTarget.dataset['prevpage']
+    const endlessUrl = new URL(this.paginationTarget.href)
+    // endlessUrl.searchParams.set("endless", prevPageValue)
+    endlessUrl.searchParams.append("endless", prevPageValue)
     // async fetch
-    await get(endlessUrl.toString(), {responseKind: 'turbo-stream'});
-    // not busy
-    this.requestValue = false;
+    await get(endlessUrl.toString(), {responseKind: 'turbo-stream'})
+    this.firstLinkAddIfNeeded()
+    this.requestValue = false
+  }
+
+  intersectionChanged = (entry) => {
+    if (entry.isIntersecting && this.hasPaginationTarget) {
+      this.loaderTarget.classList.add("show")
+      mOld = new Date().getTime();
+      running = true      // timer start
+      // clearInterval(intersections.get(entry.target))
+      count = 2500        // set timer duration (2500 -> 2.5 sec)
+      this.draw()
+      intersections.set(entry.target, setInterval(() => {
+        this.loaderTarget.classList.remove("show");
+        running = false   // timer stop
+        // console.log('Infinity scroll fired!')
+        this._loadMore()
+      }, 2500))
+    } else if (!entry.isIntersecting && intersections.get(entry.target) != null) {
+      this.loaderTarget.classList.remove("show");
+      running = false     // timer stop
+      // console.log('Timeout infinity scroll disabled!')
+      clearInterval(intersections.get(entry.target))
+    }
+  }
+
+  callback = (entries, observer) => {
+    entries.forEach(entry => this.intersectionChanged(entry))
   }
 }
